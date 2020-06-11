56ec2bd1cd65dd0c07cc36549214e20d73279e02
Sukbeom Kim <sukbeom.kim@gmail.com>
2020-06-12 01:11:22 +0900

    [upg] Add a license in README file

--- helm-eyebrowse.el
+++ helm-eyebrowse.el
@@ -5,12 +5,12 @@
   (dolist (item (eyebrowse--get 'window-configs))
     (push (format"%s: %s" (first item) (car (last item))) eyebrowse-winlist))
   (setq eyebrowse-winlist (reverse eyebrowse-winlist))
-  (setq some-helm-source
+  (setq eyebrowse-helm-source
 	'((name . "Select window number to open")
           (candidates . eyebrowse-winlist)
           (action . (lambda (candidate)
 		      (eyebrowse-switch-to-window-config (string-to-number (substring candidate 0 1)))))))
 
-  (helm :sources '(some-helm-source)))
+  (helm :sources '(eyebrowse-helm-source)))
 
 (provide 'helm-eyebrowse)
